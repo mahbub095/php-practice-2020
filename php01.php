653acd081e5476f3640d7d3a6812376e582ebb10
mahbub095 <mahbub.095@gmail.com>
2020-07-17 17:50:33 +0600

    Variable

---
+++ php01.php
@@ -0,0 +1,14 @@
+<?php
+define('PI',3.1416);
+//echo"hello world12";
+$name ='Bangladesh';
+$age =24;
+$word ="age";
+echo $$word;
+echo "\n";
+echo 'Hello '.$name;
+echo "\n";
+
+ 
+//echo "Value of PI ={PI}".PI;
+echo constant('PI');
