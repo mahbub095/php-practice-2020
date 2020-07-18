daea5e361a2c495d8395b018128e8b0a4c36ba8b
mahbub095 <mahbub.095@gmail.com>
2020-07-18 17:38:41 +0600

    Variable Capital git add .

--- php01.php
+++ php01.php
@@ -9,6 +9,14 @@ echo "\n";
 echo 'Hello '.$name;
 echo "\n";
 
- 
-//echo "Value of PI ={PI}".PI;
-echo constant('PI');
+// $empty =null;
+// echo "Value of PI ={PI}".PI;
+// echo constant('PI');
+// echo $empty;
+
+// number addition
+
+$num1 =12;
+$num2 =22;
+$sum = $num1+$num2;
+echo $sum;
---
+++ variable.php
@@ -0,0 +1,5 @@
+<?php
+$name ="Mahbubur";
+$lname ="Rahman";
+$output=sprintf("My name is %s , %s",$name,$lname);
+echo strtoupper($output);
