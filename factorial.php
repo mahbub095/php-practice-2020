364cb261825b76ad974916d74a57e5db20f83294
mahbub095 <mahbub.095@gmail.com>
2020-07-20 23:00:15 +0600

    Factorial

---
+++ factorial.php
@@ -0,0 +1,8 @@
+<?php  
+$factorial = 1;  
+for ($i=1; $i<=5; $i++)   
+{  
+  $factorial = $factorial * $i;  
+}  
+echo "Factorial of $i, is $factorial";  
+?> 
---
+++ forloop.php
@@ -0,0 +1,17 @@
+<?php
+for($i=0;$i<=10;$i++){
+    echo $i;
+    echo PHP_EOL;
+    // for($j=0;$j<$i;$j++){
+    //     echo "*";
+    
+    // }
+    // for($i=10; $i>0;$i--){
+    //     echo $i;
+    //    echo PHP_EOL;
+    // }
+
+    if($i==6){
+    break;
+    }
+    }
