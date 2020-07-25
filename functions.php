c930119c5e032c947ced61db38f44b0f1285068b
mahbub095 <mahbub.095@gmail.com>
2020-07-25 22:53:14 +0600

    php type check

--- functions.php
+++ functions.php
@@ -5,10 +5,19 @@ function isEven($n){
 
     }return false;
 }
-$x=13;
-if(isEven($x)){
-    echo "{$x} is an even Number";
-}
-    else{
-        echo "{$x} is an odd Number";
+// $x=13;
+// if(isEven($x)){
+//     echo "{$x} is an even Number";
+// }
+//     else{
+//         echo "{$x} is an odd Number";
+//     }
+
+function factorial( int $n){
+    $result = 1;
+    for($i=$n;$i>1;$i--){
+        $result *=$i;
+
     }
+    return $result;
+}
---
+++ phpfunctions.php
@@ -0,0 +1,5 @@
+<?php
+include_once"functions.php";
+$x=5;
+//$x = "abcd";
+echo "Factorial of {$x} is ".factorial ($x);
