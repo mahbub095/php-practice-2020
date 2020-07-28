c1df40cda88a5b8d4e917e8fb26cf1c8eb634710
mahbub095 <mahbub.095@gmail.com>
2020-07-29 01:38:39 +0600

    assoc

---
+++ arrayfunction/assoc.php
@@ -0,0 +1,11 @@
+<?php
+$array1 = array(
+    "a" => "green",
+    "b" => "brown",
+    "c" => "blue", "red");
+
+$array2 = array("a" => "green", "yellow", "red");
+//$array2 = array("a" => "a", "a", "a");
+$result = array_diff_assoc($array1, $array2);
+print_r($result);
+?>
