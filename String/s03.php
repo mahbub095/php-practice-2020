b273d3c769a5bae49666cab6cb07f7eeeeee24a4
mahbub095 <mahbub.095@gmail.com>
2020-08-15 20:16:02 +0600

    Character access

---
+++ String/s03.php
@@ -0,0 +1,10 @@
+<?php
+$string ="Hello World";
+//echo $string[0];
+//echo $string[-5];
+$length = strlen($string);
+//echo substr($string ,1 ,4);
+//echo substr($string,$length-3);
+
+echo PHP_EOL;
+echo substr($string, -7,-4);
