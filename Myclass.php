8fad4b2b9a890881ca86013430ad5354c58eeaa8
Mahbub <mahbub.095@gmail.com>
2020-08-24 23:59:59 +0600

    interface

---
+++ Myclass.php
@@ -0,0 +1,14 @@
+<?php 
+  
+class MyClassName implements MyInterfaceName{ 
+   public  function methodA() {  
+  
+     // method A implementation 
+   }  
+   public  function methodB(){  
+  
+     // method B implementation 
+   }  
+} 
+  
+?> 
---
+++ interface.php
@@ -0,0 +1,6 @@
+<?php
+interface MyInterfaceName{
+    public function methodA();
+    public function methodB();
+}
+?>
