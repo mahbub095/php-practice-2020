2676d1e47bfcd18984ee4a7580dd3ebdebcc57d6
Mahbub <mahbub.095@gmail.com>
2020-08-29 13:30:00 +0600

    Method Chain

---
+++ OOP-Advanced/methodchain.php
@@ -0,0 +1,35 @@
+<?php
+class StringUtility{
+    private $string;
+    private $search;
+    function __construct($string){
+        $this->string =$string;
+    }
+    function search($string){
+        $this->search = $string;
+        return $this;
+    }
+    function replace($string){
+        if(!isset($this->search)){
+            throw new Exception ("Nothing To Replace");
+        }
+        $this->string = str_replace($this->search,$string,$this->string);
+        return $this;
+    }
+    function upperCase(){
+        $this->string = strtoupper($this->string);
+        return $this;
+    }
+    function lowercase(){
+        $this->string = strtolower($this->string);
+        return $this;
+    }
+    function print(){
+        echo $this->string;
+    }
+}
+
+
+$s = new StringUtility("Hello  World");
+//$s->replaceString("search" ,"replace");
+$s->search("World")->replace("Earth")->upperCase()->print();
